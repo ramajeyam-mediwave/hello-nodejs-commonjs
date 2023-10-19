0f7785a3e08afe97550baae1218bbb2036fcd82b
ramajeyam-mediwave <ramajeyam@mindwaveventures.com>
2023-10-19 17:38:17 +0530

    add get and post routes

---
+++ app/config.js
@@ -0,0 +1,5 @@
+const config = {
+  port: 3456,
+};
+
+module.exports = config;
--- app/index.js
+++ app/index.js
@@ -1,8 +1,53 @@
-// const stringHelper = require("./utils/stringHelper");
-const { stringLength } = require("./utils/stringHelper");
+const express = require("express");
+const morgan = require("morgan");
+const config = require("./config");
 
-console.log("Hello world");
+const app = express();
+app.use(morgan("combined"));
 
-// const l = stringHelper.stringLength("12345");
-const l = stringLength("12345");
-console.log(l);
+app.use(express.json());
+
+const favs = [];
+
+// reading READ
+app.get("/", (req, res) => {
+  return res.json({
+    data: {
+      favs,
+    },
+  });
+});
+
+// add data CREATE
+app.post("/", (req, res) => {
+  //   console.log("==> ", req.body);
+  //   console.log("==> ", req.method);
+  //   console.log("==> ", req.headers);
+  //   console.log("==> ", req.path);
+
+  favs.push({
+    id: new Date().getTime(),
+    text: req.body.text,
+  });
+  return res.json({
+    message: "added data",
+  });
+});
+
+// Update
+app.put("/", (req, res) => {
+  return res.json({
+    message: "PUT route",
+  });
+});
+
+// DELETE
+app.delete("/", (req, res) => {
+  return res.json({
+    message: "DELETE route",
+  });
+});
+
+app.listen(config.port, () => {
+  console.log(`Server running on port ${config.port}`);
+});
