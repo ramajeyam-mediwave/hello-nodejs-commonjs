fa2bb21b8b0ca8d4626d9c33a309463e587ea5d7
ramajeyam-mediwave <ramajeyam@mindwaveventures.com>
2023-10-19 17:06:15 +0530

    init commit

---
+++ app/index.js
@@ -0,0 +1,8 @@
+// const stringHelper = require("./utils/stringHelper");
+const { stringLength } = require("./utils/stringHelper");
+
+console.log("Hello world");
+
+// const l = stringHelper.stringLength("12345");
+const l = stringLength("12345");
+console.log(l);
